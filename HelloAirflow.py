f25ae7fd093527828e6c61dbaf16d43491da3f6b
KartheekTide <KartheekTide>
2022-08-18 07:09:12 +0000

    added airflow Files

---
+++ HelloAirflow.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+dbutils.widgets.text("greeting", "world", "Greeting")
+greeting = dbutils.widgets.get("greeting")
+
+# COMMAND ----------
+
+print("hello {}".format(greeting))
+
+# COMMAND ----------
+
+# MAGIC %run ./HelloAirflow2
---
+++ HelloAirflow2.py
@@ -0,0 +1,2 @@
+# Databricks notebook source
+print('Airflow Second Notebook')
