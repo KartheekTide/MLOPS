ae6f1c8bf539c18623b6d5699866a06c9e74446a
KartheekTide <KartheekTide>
2022-08-22 13:07:19 +0000

    Added New Files

---
+++ DataIngest.py
@@ -0,0 +1,52 @@
+# Databricks notebook source
+from sklearn import datasets
+import pandas as pd
+import datetime
+import os
+
+# COMMAND ----------
+
+data = datasets.load_breast_cancer()
+
+# COMMAND ----------
+
+# Read the DataFrame, first using the feature data
+df = pd.DataFrame(data.data, columns=data.feature_names)
+# Add a target column, and fill it with the target data
+df['target'] = data.target
+# Show the first five rows
+df.rename(columns = lambda x : x.replace(' ', '_'),inplace=True)
+df.head()
+
+# COMMAND ----------
+
+df =spark.createDataFrame(df)
+
+# COMMAND ----------
+
+dataset_name='mlops_'+datetime.datetime.today().strftime('%Y%m%d')
+
+# COMMAND ----------
+
+df.write.format('parquet').save(os.path.join('dbfs:/tmp/',dataset_name))
+
+# COMMAND ----------
+
+def load_dataset(dataset_name):
+  return spark.read.parquet('dbfs:/tmp/'+dataset_name)
+
+# COMMAND ----------
+
+dataset_renames= 'dbfs:/tmp/'+dataset_name
+
+# COMMAND ----------
+
+df_reload=load_dataset(dataset_name)
+
+# COMMAND ----------
+
+df_reload.show(10)
+
+# COMMAND ----------
+
+
--- DataIngestionTecton.py
+++ DataIngestionTecton.py
@@ -8,4 +8,24 @@ feature_service.feature_views
 
 # COMMAND ----------
 
+feature_view = tecton.get_feature_view("invoice_transaction_datetime_features")
+
+# COMMAND ----------
+
+result_dataframe = feature_view.run()
+#print(display(result_dataframe.to_spark()))
+
+# COMMAND ----------
+
+result_dataframe
+
+# COMMAND ----------
+
+from datetime import datetime, timedelta
+start_time = datetime.today() - timedelta(days=2)
+results = feature_view.get_historical_features(start_time=start_time)
+display(results)
+
+# COMMAND ----------
+
 
