5873ead23d8a3205cfadba3ec882ca7df43dd05d
KartheekTide <KartheekTide>
2022-08-23 12:53:19 +0000

    modified feature engineering file.

--- FeatureEngineering.py
+++ FeatureEngineering.py
@@ -32,4 +32,62 @@ df.dtypes
 
 # COMMAND ----------
 
+from sklearn import preprocessing
+import pandas as pd
+import datetime
+import os
+
+# COMMAND ----------
+
+X = df.iloc[:,:-1]
+y = df.iloc[:,-1]
+
+# COMMAND ----------
+
+mm_scaler = preprocessing.MinMaxScaler()
+preprocess = mm_scaler.fit_transform(X)
+
+
+# COMMAND ----------
+
+preprocess = pd.DataFrame(data=preprocess,columns=X.columns)
+
+# COMMAND ----------
+
+preprocess['target'] = y
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+preprocess = spark.createDataFrame(preprocess)
+
+# COMMAND ----------
+
+dataset_name='final_'+datetime.datetime.today().strftime('%Y%m%d')
+
+# COMMAND ----------
+
+pandasDF = pandasDF.assign(final_dataset_path=[dataset_name])
+
+# COMMAND ----------
+
+pandasDF
+
+# COMMAND ----------
+
+preprocess.write.format('parquet').save(os.path.join('dbfs:/tmp/',dataset_name))
+
+# COMMAND ----------
+
+conf = spark.createDataFrame(pandasDF)
+
+# COMMAND ----------
+
+conf.write.mode('overwrite').parquet(os.path.join('dbfs:/tmp/','conf'))
+
+# COMMAND ----------
+
 
