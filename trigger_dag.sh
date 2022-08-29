1cd7a332a0036b38b1f68312b915224c0a7de073
KartheekTide <89909463+KartheekTide@users.noreply.github.com>
2022-08-29 18:56:42 +0530

    Update trigger_dag.sh

--- trigger_dag.sh
+++ trigger_dag.sh
@@ -1,5 +1,5 @@
-$airflow_server = locahost:8080
-curl --location --request POST 'http://localhost:8080/api/v1/dags/example_databricks_repos_operator_v2/dagRuns' \
+#!/bin/bash
+curl --location --request POST 'http://'AIRFLOW_SERVER'/api/v1/dags/example_databricks_repos_operator_v2/dagRuns' \
 --header 'Authorization: Basic YWRtaW46dGlkZQ==' \
 --header 'Content-Type: application/json' \
 --header 'Cookie: session=.eJwNy0EKgCAQBdC7zLqNaVpeRmz6Q5BZqK2iu-f2wXspSEHdyUtMFQOFG-WMGbmRb-XpwrVIaNeBTJ7UqLHaUYl2s1p4AqxRZnFu1Rw3YbazgbZCA6WLY0I_PX4_EAog2A.YwcyNQ.DUrzhmoY16JL7mP6FVRYBFfHndM' \
